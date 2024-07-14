053fa3b559a04d2534f5ffa76889d9296bbc7121
MarcelWeidum <marcelweidum@live.nl>
2024-07-14 20:33:57 +0200

    Reproduce filament bug

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Customer;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Section;
+use Filament\Support\Enums\ActionSize;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\Actions\Action;
+use App\Filament\Resources\CustomerResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\CustomerResource\RelationManagers;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Section')
+                    ->headerActions([
+                        Action::make('refresh')
+                            ->icon('heroicon-o-arrow-path')
+                            ->color('warning')
+                            ->size(ActionSize::Small)
+                            ->hidden(fn ($record) => !$record)
+                            ->requiresConfirmation()
+                            ->action(function($record, $livewire) {
+                                $record = $record->fresh();
+                                $livewire->refreshFormData(['name', 'image']);
+
+                                Notification::make()
+                                    ->title('Refreshed data')
+                                    ->success()
+                                    ->send();
+                            }),
+                    ])
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+                        FileUpload::make('image')
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
